125371340f895bee51707af35eb385507c2df446
Tom Montgomery <montgomery3681@gmail.com>
2023-01-16 22:24:04 -0700

    Initial Commit

---
+++ M5L8_navLinkPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M5L8_navLinkPlus
+//
+//  Created by tom montgomery on 1/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M5L8_navLinkPlus/M5L8_navLinkPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5L8_navLinkPlusApp.swift
+//  M5L8_navLinkPlus
+//
+//  Created by tom montgomery on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct M5L8_navLinkPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
